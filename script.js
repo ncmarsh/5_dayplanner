9ca7477d49390ab9267695037fbd380c1cd9c37d
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-03 16:47:01 -0700

    created on click event to set text input to localstorage on first time block

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let currentHour = moment().calendar();
 // If plannerHour is less than currentHour, the planner is in the past
 if (plannerHour < currentHour) {
     textAreaEl.addClass("past");
-    console.log(currentHour);
-    console.log(plannerHour);
+    // console.log(currentHour);
+    // console.log(plannerHour);
 }
 
 // If plannerHour is equal to the currentHour, the planner is in the present
@@ -22,3 +22,16 @@ if (plannerHour === currentHour) {
 if (plannerHour > currentHour) {
     textAreaEl.addClass("future");
 }
+
+// Save button on click saves content from text area to localstorage
+// let plannerStorageArr = JSON.parse(localStorage.getItem("plans")) || [];
+let plannerStorageArr = [];
+
+let saveBtnEl = $(".fa-save");
+
+saveBtnEl.on("click", function(){
+    let plannerInputEl = $("#textArea").val();
+    console.log(plannerInputEl);
+    plannerStorageArr.push({hourblock: plannerHour, plans: plannerInputEl});
+    localStorage.setItem("plans", JSON.stringify(plannerStorageArr));
+})
