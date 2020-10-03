cf279e2d589574264e4450f83d5e549da54c99c8
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-02 20:09:14 -0700

    added if statements to script to determine if time block is in the past, present or future

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // Displays the current date at the top of the page
 $("#currentDay").text(moment().format('LLLL'));
+
+// If hour is equal to current time, it will be colored red
+let textAreaEl = $("textarea");
+let plannerHour = $(".hour").val();
+let currentHour = moment().calendar();
+
+// If plannerHour is less than currentHour, the planner is in the past
+if (plannerHour < currentHour) {
+    textAreaEl.addClass("past");
+    console.log(currentHour);
+    console.log(plannerHour);
+}
+
+// If plannerHour is equal to the currentHour, the planner is in the present
+if (plannerHour === currentHour) {
+    textAreaEl.addClass("present");
+}
+
+// If plannerHour is greater than currentHour, the planner is in the future
+if (plannerHour > currentHour) {
+    textAreaEl.addClass("future");
+}
