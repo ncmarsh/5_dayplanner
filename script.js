f8bdfc862b8e7c640dd6d4b87c83d725104e9444
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-04 15:02:33 -0700

    changed work hours arr into numbers, if statement to determine AM or PM, and if statements to compare the id to the current hour to determine past, present, future

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 $("#currentDay").text(moment().format('LLLL'));
 
 // Takes just the hour of the current time in the 24hr clock format
-let currentHour = parseInt(moment().format('H'));
-console.log(currentHour);
+const currentHour = parseInt(moment().format('H'));
+// console.log(currentHour);
 
 // Business hours array
-let workHoursArr = ["8AM", "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM"];
+let workHoursArr = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 // For each work hour, a row with 3 columns is created, with the hour, text input and save icon
 workHoursArr.forEach(function(i) {
@@ -16,70 +16,61 @@ workHoursArr.forEach(function(i) {
     let saveBtnColEl = $("<div>").attr("class", "col-md-1 saveBtn");
     let saveIconEl = $("<i>").attr("class", "fas fa-save");
 
-    hourColEl.text(i);
+    // console.log(i);
+
+    if (i < 12) {
+        hourColEl.text(i + ":00 AM");
+    } else if (i === 12) {
+        hourColEl.text(i + ":00 PM");
+    } else {
+        hourColEl.text(i - 12 + ":00 PM");
+    }
+
+    textColEl.attr("id", i);
     $(".time-block").append(rowEl);
     rowEl.append(hourColEl).append(textColEl).append(saveBtnColEl);
     saveBtnColEl.append(saveIconEl);
+    // need to add id's to save button, to trigger looking at previous sibling = text area
 })
 
-// Take the number value of the hour class and assign to variable rowHour
-let rowHour = parseInt($(".hour").text());
-
-let textAreaEl = $("textarea");
-let saveBtnEl = $(".fa-save");
-
-// For each hour class 
-$(document).ready(function(){
-    $(".hour").each(function(){
-        console.log(rowHour);
-        console.log(currentHour);
-        if (rowHour < currentHour) {
-            textAreaEl.addClass("past");
-        }
-        if (rowHour === currentHour) {
-            textAreaEl.addClass("present");
-        }
-        if (rowHour > currentHour) {
-            textAreaEl.addClass("future");
-        }
-    })
+// For each text area tag 
+$("textarea").each(function(i){
+    let rowHourId = parseInt($(this).attr("id"));
+    // If the text area Id is less than the currentHour, the row is in the past
+    if (rowHourId < currentHour) {
+        $(this).addClass("past");
+    }
+    // If the text area Id is the currentHour, the row is in the present
+    if (rowHourId === currentHour) {
+        $(this).addClass("present");
+    }
+    // If the text area Id is greater than the currentHour, the row is in the future
+    if (rowHourId > currentHour) {
+        $(this).addClass("future");
+    }
 })
 
 
-
-// If hour is equal to current time, it will be colored red
-// If plannerHour is less than currentHour, the planner is in the past
-// if (plannerHour < currentHour) {
-//     textAreaEl.addClass("past");
-//     // console.log(currentHour);
-//     // console.log(plannerHour);
-// }
-
-// If plannerHour is equal to the currentHour, the planner is in the present
-// if (plannerHour === currentHour) {
-//     textAreaEl.addClass("present");
-// }
-
-// If plannerHour is greater than currentHour, the planner is in the future
-// if (plannerHour > currentHour) {
-//     textAreaEl.addClass("future");
-// }
 
 // Save button on click saves hour and content from text area to localstorage
 let plannerStorageArr = JSON.parse(localStorage.getItem("plans")) || [];
 // let plannerStorageArr = [];
 
-saveBtnEl.on("click", function(){
-    let rowInputEl = $("#textArea").val();
+let saveBtnEl = $(".fa-save");
 
-    console.log(rowHour);
+saveBtnEl.on("click", function(event){
+    // let rowHourSpot = parseInt(event.target.siblings(id));
+    let rowHourSpot = $(this).parent().siblings().find(".hour");
+    console.log(rowHourSpot);
+    let rowInputEl = $("textArea").val();
+
+    // console.log(rowHour);
     console.log(rowInputEl);
-    plannerStorageArr.push({hourblock: rowHour, plans: rowInputEl});
-    localStorage.setItem("plans", JSON.stringify(plannerStorageArr));
+    // plannerStorageArr.push({hourblock: rowHour, plans: rowInputEl});
+    // localStorage.setItem("plans", JSON.stringify(plannerStorageArr));
 })
 
 
-// Need it to console log the time in the time block
-// Need it to register what time is when - for loop
+
 // Need it to set item hourblock in local storage from the time in the time block
 // Need to figure out how to save the specific timeblocks in local storage
