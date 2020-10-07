6a4027d7c4f3a5c9f71238f01288c9fe298019da
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-06 22:02:01 -0700

    created setPlans function to getItem from localStorage and set val() to each row

--- script.js
+++ script.js
@@ -1,165 +1,89 @@
-// Displays the current date and time at the top of the page
-$("#currentDay").text(moment().format('LLLL'));
+$(document).ready(function() {
+    // Displays the current date and time at the top of the page
+    $("#currentDay").text(moment().format('LLLL'));
 
-// Takes just the hour of the current time in the 24hr clock format
-const currentHour = parseInt(moment().format('H'));
-// console.log(currentHour);
+    // Takes just the hour of the current time in the 24hr clock format
+    const currentHour = parseInt(moment().format('H'));
+    // console.log(currentHour);
 
-// Business hours array, 24 hour clock
-let workHoursArr = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+    // Business hours array, 24 hour clock
+    let workHoursArr = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-// For each work hour, a row with 3 columns is created, to hold the hour, text input and save icon
-workHoursArr.forEach(function(i) {
-    let rowEl = $("<div>").attr("class", "row");
-    let hourColEl = $("<div>").attr("class", "col-md-2 hour");
-    let textColEl = $("<textarea>").attr("class", "col-md-8");
-    let saveBtnColEl = $("<div>").attr("class", "col-md-1 saveBtn");
-    let saveIconEl = $("<i>").attr("class", "fas fa-save");
+    // For each work hour, a row with 3 columns is created, to hold the hour, text input and save icon
+    workHoursArr.forEach(function(i) {
+        let rowEl = $("<div>").attr("class", "row");
+        let hourColEl = $("<div>").attr("class", "col-md-2 hour");
+        let textColEl = $("<textarea>").attr("class", "col-md-8");
+        let saveBtnColEl = $("<div>").attr("class", "col-md-1 saveBtn");
+        let saveIconEl = $("<i>").attr("class", "fas fa-save");
 
-    // Take the time 24 hour clock and converts to 12 hr clock and adds either AM or PM
-    if (i < 12) {
-        hourColEl.text(i + ":00 AM");
-    } else if (i === 12) {
-        hourColEl.text(i + ":00 PM");
-    } else {
-        hourColEl.text(i - 12 + ":00 PM");
+        // Take the time 24 hour clock and converts to 12 hr clock and adds either AM or PM
+        if (i < 12) {
+            hourColEl.text(i + ":00 AM");
+        } else if (i === 12) {
+            hourColEl.text(i + ":00 PM");
+        } else {
+            hourColEl.text(i - 12 + ":00 PM");
+        }
+
+        // Sets an id to each text area tag
+        textColEl.attr("id", i);
+
+        $(".time-block").append(rowEl);
+        rowEl.append(hourColEl).append(textColEl).append(saveBtnColEl);
+        saveBtnColEl.append(saveIconEl);
+    })
+
+    // To set the background color for each text area tag 
+    $("textarea").each(function(i) {
+        let rowHourId = parseInt($(this).attr("id"));
+        // If the text area Id is less than the currentHour, the row is in the past
+        if (rowHourId < currentHour) {
+            $(this).addClass("past");
+        }
+        // If the text area Id is the currentHour, the row is in the present
+        if (rowHourId === currentHour) {
+            $(this).addClass("present");
+        }
+        // If the text area Id is greater than the currentHour, the row is in the future
+        if (rowHourId > currentHour) {
+            $(this).addClass("future");
+        }
+    })
+
+    // When the save button is clicked
+    $(".fa-save").on("click", function(){
+        // Takes the Id of that row's text area
+        let hourBlockEl = $(this).parent().siblings("textarea").attr("id");
+        // Takes the input of that row's content
+        let planInputEl = $(this).parent().siblings("textarea").val();
+
+        console.log(hourBlockEl);
+        console.log(planInputEl);
+
+        localStorage.setItem(hourBlockEl, planInputEl);
+
+    })
+
+    // When clear day button is clicked, local storage will clear
+        // But isn't clearing planner
+    $("#clear-day").on("click", function() {
+        localStorage.clear();
+
+        setPlans();
+    })
+
+    function setPlans () {
+        $("#8").val(localStorage.getItem("8"));
+        $("#9").val(localStorage.getItem("9"));
+        $("#10").val(localStorage.getItem("10"));
+        $("#11").val(localStorage.getItem("11"));
+        $("#12").val(localStorage.getItem("12"));
+        $("#13").val(localStorage.getItem("13"));
+        $("#14").val(localStorage.getItem("14"));
+        $("#15").val(localStorage.getItem("15"));
+        $("#16").val(localStorage.getItem("16"));
     }
 
-    // Sets an id to each text area tag
-    textColEl.attr("id", i);
-
-    $(".time-block").append(rowEl);
-    rowEl.append(hourColEl).append(textColEl).append(saveBtnColEl);
-    saveBtnColEl.append(saveIconEl);
-})
-
-// To set the background color for each text area tag 
-$("textarea").each(function(i) {
-    let rowHourId = parseInt($(this).attr("id"));
-    // If the text area Id is less than the currentHour, the row is in the past
-    if (rowHourId < currentHour) {
-        $(this).addClass("past");
-    }
-    // If the text area Id is the currentHour, the row is in the present
-    if (rowHourId === currentHour) {
-        $(this).addClass("present");
-    }
-    // If the text area Id is greater than the currentHour, the row is in the future
-    if (rowHourId > currentHour) {
-        $(this).addClass("future");
-    }
-})
-
-
-
-// Save button on click saves hour and content from text area to localstorage
-// let plannerStorageArr = JSON.parse(localStorage.getItem("plans")) || [];
-// let plannerStorageArr = [];
-
-// If there are objects in local storage
-   // For each object in local storage, it will look to see if it has an hour block equal to a text area id
-    // If it does, it will set the text content from the i.task
-// if (plannerStorageArr) {
-//     console.log("yes");
-
-    // let savePlans = JSON.parse(localStorage.getItem("plans"));
-    // console.log(savePlans);
-
-    // plannerStorageArr.sort (function (a,b) {
-    //     return a.hourblock - b.hourblock;
-    // })
-
-    // plannerStorageArr.forEach(function(i) {
-    //     let saveTextId = $("textarea").attr("id");
-    //     if (i.hourblock === saveTextId) {
-    //         console.log("yay");
-    //         console.log(saveTextId);
-    //         console.log(i.task);
-    //         $(saveTextId).val(i.task);
-    //     }
-
-        // $("textarea").each(function(i) {
-        //     console.log(i);
-        //     let saveTextId = $("textarea").attr("id");
-        //     if (savePlans !== null && saveTextId === savePlans[i].hourblock) {
-        //         console.log("winner");
-        //         $(this).text(savePlans[i].task);
-    // })
-
-    // plannerStorageArr.forEach(function(i) {
-    //     console.log(i);
-    //     let saveTextId = $("textarea").attr("id");
-    //     if (i.hourblock === saveTextId) {
-    //         console.log(saveTextId);
-    //         console.log(i.task);
-            
-    //         let saveTextVal = $("textarea").val();
-    //         console.log("yes");
- 
-    //     }
-    // })
-// }
-
-// if (plannerStorageArr) {
-//     console.log("yes");
-
-//     let savePlans = JSON.parse(localStorage.getItem("plans"));
-//     console.log(savePlans);
-
-//     $("textarea").each(function(i) {
-//         console.log(i);
-//         let saveTextId = $("textarea").attr("id");
-//         if (savePlans !== null && saveTextId === savePlans[i].hourblock) {
-//             console.log("winner");
-//             $(this).text(savePlans[i].task);
-//         } 
-//     })
-// }
-
-
-// When the save button is clicked
-$(".fa-save").on("click", function(){
-    // Takes the Id of that row's text area
-    let hourBlockEl = $(this).parent().siblings("textarea").attr("id");
-    // Takes the input of that row's content
-    let planInputEl = $(this).parent().siblings("textarea").val();
-
-    console.log(hourBlockEl);
-    console.log(planInputEl);
-
-    // Pushes the Id and Content into an object
-    // plannerStorageArr.push({hourblock: planTimeEl, task: planInputEl});
-    
-    // Turns the object into a string and sets to local storage
-    // localStorage.setItem("plans", JSON.stringify(plannerStorageArr));
-    // console.log(plannerStorageArr);
-    
-    localStorage.setItem(hourBlockEl, planInputEl);
-
-    // let savePlansEl = $(this).parent().siblings("textarea").text();
-    // let retrPlansEl = localStorage.getItem("planInputEl");
-    // let retrPlansEl = localStorage.getItem("planInputEl");
-    // $(planSaveEl).text(retrPlansEl);
-
-
-})
-
-$("textarea").each(function() {
-
-    for (let i = 0; i < localStorage.length; i++) {
-        
-    }
-
-})
-
-
-
-// When I hit the save button, it will pull it's specific ID and go to the parent sibling ID to save the content from that specific psID to local storage item
-// If editing text, it needs to replace the hourblock if it exists
-// When I refresh the page, the local storage info needs to PERSIST!
-
-
-// Add reset day button
-$("#clear-day").on("click", function() {
-    localStorage.clear();
-})
+    setPlans();
+});
